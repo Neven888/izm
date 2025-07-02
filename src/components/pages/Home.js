2ee0753bf6a2e635707c9b1abcd158e0fa732d0b
Neven Radanovic <neven.radanovic@hotmail.com>
2025-07-02 11:46:02 +0200

    latest blog update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,30 +2,13 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import LatestPostsSlider from "../parts/LatestPostsSlider";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
         <>
-        <Slider {...{arrows: true, infinite: true, speed: 500, slidesToShow: 1, slidesToScroll: 1, autoplay: true, autoplaySpeed: 3000}} className="slider">
-        <div className="slide">
-          <img src="https://placehold.co/1428x671" alt="Image 1" className="slajd" />
-          <div className="title">LOREM IPSUM IS SIMPLYS</div>
-          <div className="subtitle">Lorem Ipsum is simply dummy text of the printing</div>
-          <div className="button rounded-pill brojgumb">(123) 456-7890</div>
-        </div>
-        <div className="slide">
-          <img src="https://placehold.co/1428x671" alt="Image 1" className="slajd" />
-          <div className="title">LOREM IPSUM IS SIMPLYS</div>
-          <div className="subtitle">Lorem Ipsum is simply dummy text of the printing</div>
-          <div className="button rounded-pill brojgumb">(123) 456-7890</div>
-        </div>
-        <div className="slide">
-          <img src="https://placehold.co/1428x671" alt="Image 1" className="slajd" />
-          <div className="title">LOREM IPSUM IS SIMPLYS</div>
-          <div className="subtitle">Lorem Ipsum is simply dummy text of the printing</div>
-          <div className="button rounded-pill brojgumb">(123) 456-7890</div>
-        </div>
-      </Slider>
+        <LatestPostsSlider />
     <section className="prvi-section py-4">
       <div className="container">
         <div className="row justify-content-center">
@@ -36,31 +19,34 @@ const Home = () => {
           </p>
           <p>Lorem Ipsum is Simply Dummy</p>
           <p>Lorem Ipsum is Simply Dummy Text Of The Printing Type</p>
-          <div class="button rounded-pill m-3">YOURS MORE</div>
+          <div className="button rounded-pill m-3">YOURS MORE</div>
         </div>
       </div>
       </div>
     </section>
 
-    <section class="drugi-section py-5">
-      <div class="container drugi-section">
-        <div class="row">
-        <div class="col-6">
-          <h1>LOREM IPSUM IS SIMPLY DO TEXT OF THE PRINTING TYPESETTING INDUSTRY.</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy texm h,h t ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          <div class="button rounded-pill plavigumb p-1">YOURS MORE</div>
+    <section className="drugi-section py-5">
+       <div className="overlay"></div>
+      <div className="container drugi-section-content">
+        <div className="row">
+        <div className="col-6">
+          <h1>O NAMA</h1>
+          <p>Udruga pivara Istre i Kvarnera osnovana je s ciljem okupljanja ljubitelja pivarstva i razmjene znanja i vještina u proizvodnji piva. 
+Naša misija je promovirati pivarstvo kao hobi, poticati zajednička druženja te unaprjeđivati vještine i iskustva putem povratnih informacija kroz kušanja. Organiziranjem edukativnih predavanja, seminara, tečajeva, radionica i manifestacija, Udruga aktivno doprinosi razvoju pivarske kulture i jača društvene veze sa srodnim organizacijama. 
+S entuzijazmom gradimo zajednicu posvećenu znanju, kreativnosti i zajedničkim iskustvima u svijetu pivarstva.</p>
+          <Link className="button rounded-pill istakni" to="https://docs.google.com/forms/d/e/1FAIpQLSegyVPnlsW4zHwHLKPo2iRKhEyxnPIZHB6s35kb1xb43EoB0Q/viewform" target="_blank">POSTANI ČLAN</Link>
         </div>
-        <div class="col-6 text-center">
-          <img src="https://placehold.co/400x300" />
+        <div className="col-6 text-center">
+          <img src="img/piklogo.png" className="img-fluid" />
         </div>
       </div>
       </div>
     </section>
 
     <section>
-      <div class="container">
+      <div className="container">
         <div className="row text-center py-5">
-          <h1 className="pb-4 mb-4">LOREM IPSUM IS SIMPLY DUMMY</h1>
+          <h1 className="pb-4 mb-4">GALERIJA</h1>
           <Slider {...{dots: true, arrows: false, infinite: true, speed: 500, slidesToShow: 5, slidesToScroll: 5, initialSlide: 0,
     responsive: [
       {
---
+++ src/components/parts/LatestPostsSlider.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const LatestPostsSlider = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("https://wp1.edukacija.online/backend/wp-json/wp/v2/posts?_embed&author=6&per_page=3");
+        if (!res.ok) throw new Error("Greška pri dohvaćanju postova");
+        const data = await res.json();
+        setPosts(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (error) return <p>Greška: {error}</p>;
+  if (!posts.length) return <p>Učitavanje...</p>;
+
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    speed: 600,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="slick-carousel">
+      <Slider {...settings}>
+        {posts.map((post) => (
+          <div className="slide" key={post.id}>
+            <img
+              src={post._embedded["wp:featuredmedia"]?.[0]?.source_url}
+              alt={post.title.rendered}
+              className="slajd"
+            />
+            <div className="overlay"></div>
+            <div
+              className="title"
+              dangerouslySetInnerHTML={{ __html: post.title.rendered }}
+            />
+            <div
+              className="subtitle"
+              dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+            />
+            <Link
+              to={`/pivarski-blog/${post.slug}`}
+              className="button rounded-pill brojgumb"
+            >
+              Pročitaj više
+            </Link>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default LatestPostsSlider;
